8d689caad42f08f7c32dc829563a929fbcc7674c
Edwin Pers <epers@ansencorp.com>
2021-03-25 10:11:48 -0400

    changed subnet

---
+++ hostaddr_port_excel.mjs
@@ -0,0 +1,8 @@
+'use strict';
+
+// don't judge me....
+
+for (let i = 1; i <= 52; i++) {
+  console.log(`1g${i}.r1t2`);
+  console.log('');
+};
--- subnets.mjs
+++ subnets.mjs
@@ -2,7 +2,7 @@
 
 // subnet without hostaddr
 // assumed to be a /24
-const subnet = '10.15.16.'
+const subnet = '10.15.18.'
 
 for (let i = 0; i < 255; i = i) {
   // print the subnet 
